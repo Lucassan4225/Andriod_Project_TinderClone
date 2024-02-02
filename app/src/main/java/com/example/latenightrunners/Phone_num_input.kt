e86dd3f2f84f9d72686745c70f864f253a7c0a0a
Lucassan4225 <145424551+Lucassan4225@users.noreply.github.com>
2024-02-02 22:28:57 +0700

    update from lucas

---
+++ app/src/main/java/com/example/latenightrunners/Phone_num_input.kt
@@ -0,0 +1,11 @@
+package com.example.latenightrunners
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Phone_num_input : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_phone_num_input)
+    }
+}
