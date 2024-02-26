af81c3e50b23be34b83b8b907caf0dd612fce6f9
Kyaw Ye Lwin@Anmol <141581004+MarioK404@users.noreply.github.com>
2024-02-26 21:46:56 +0700

    Update 2 from Anmol (Feb 1)

---
+++ app/src/main/java/com/example/latenightrunners/MoreInfoActivity.kt
@@ -0,0 +1,11 @@
+package com.example.latenightrunners
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MoreInfoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_more_info)
+    }
+}
